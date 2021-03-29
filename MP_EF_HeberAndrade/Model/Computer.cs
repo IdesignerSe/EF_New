8170d99e6e0aa9646efcb74c6968357234789d99
idesignerse <heberandrade@gmail.com>
2021-03-29 16:59:28 +0200

    Starter EF

---
+++ MP_EF_HeberAndrade/Model/Computer.cs
@@ -0,0 +1,21 @@
+﻿namespace MP_EF_HeberAndrade
+
+{
+    public class Computer : Asset
+    {
+        public Computer(string v1, string v2, int v3, int v4)
+        {
+        }
+
+        public Computer(string brand, string modelName, int purchaseDate, int inicialCost, int expiredDate, int expiredCost)
+        {
+            Brand = brand;
+            ModelName = modelName;
+            PurchaseDate = purchaseDate;
+            InicialCost = inicialCost;
+            ExpiredDate = expiredDate;
+            ExpiredCost = expiredCost;
+        }
+        public int Id { get; set; }
+    }
+}
---
+++ MP_EF_HeberAndrade/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MP_EF_HeberAndrade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var dbContext = new AssetsContext())
+            {
+
+                 //(string brand, string modelName, int purchaseDate, int inicialCost, int expiredDate, int expiredCost)
+                // new Asset("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000),
+                 dbContext.Add(new Computer("MacBook", "Pro 2018 15 inch ", 20180101, 13000, 20211201, 8000));
+                  
+                 dbContext.SaveChanges();
+                
+                 var result = dbContext.Computers.ToList()
+                                   .Select(d => d.Brand);
+                Console.WriteLine(string.Join(", ", result));;
+
+
+
+   //             Console.WriteLine("Hello World!");
+     //           using (var dbContext = new CustomTreatmentContext())
+       //         {
+         //           dbContext.Add(new Diet("Superduper diet", "Hard", 2, 200));
+           //         dbContext.SaveChanges();
+             //       var result = dbContext.Diets.ToList()
+               //         .Where(d => d.DietDaysProgram > 0)
+                 //       .Take(5)
+                   //     .OrderBy(d => d.DietDaysProgram)
+ //                       .Select(d => d.DietName);
+ //
+   //                 var resultSum = dbContext.Diets
+     //                   .Where(d => d.DietDaysProgram > 0)
+       //                 .OrderBy(d => d.DietDaysProgram)
+         //               .GroupBy(o => 1)
+           //             .Select(d =>
+             //               "Average days:" + (float)d.Sum(d => d.DietDaysProgram) / (float)Math.Max(1, d.Count()) +
+               //             "(total diets: " + d.Count() + ")"
+                 //       );
+ //                   Console.WriteLine(string.Join(", ", result));
+   //                 Console.WriteLine(string.Join(", ", resultSum));
+     //           }
+
+
+
+            }
+        }
+    }
+}
